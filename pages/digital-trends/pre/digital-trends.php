f220f51a2e36dbb771a1793851b7358a1be36dc1
mcappato <mcappato@makingsense.com>
2024-08-05 16:17:45 -0300

    chore: digital trends pages home & interna

---
+++ pages/digital-trends/pre/digital-trends.php
@@ -0,0 +1,34 @@
+<?php
+require_once('././config.php');
+require_once('././src/components/cacheSettings.php');
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <?php include('./components/digital-trends/pre/digital-trends/head.php'); ?>
+  <?php include('./components/head.php'); ?>
+</head>
+
+<body class="emms__ecommerce">
+  <?php include('./components/gtm.php'); ?>
+  <?php include('./components/navbar-unreg.php') ?>
+  <?php include('./components/share.php') ?>
+  <main>
+    <?php include('./components/digital-trends/pre/register-form.php') ?>
+    <?php include('./components/digital-trends/pre/speakers-carousel.php') ?>
+    <div class="emms__bg-dark-gradient">
+      <?php include('./components/digital-trends/pre/grid-event-types.php') ?>
+      <?php include('./components/digital-trends/pre/event-numbers.php') ?>
+    </div>
+    <?php include('./components/digital-trends/pre/benefit-icons.php') ?>
+    <?php include('./components/digital-trends/pre/premium-content.php') ?>
+    <?php include('./components/digital-trends/pre/central-video.php') ?>
+    <?php include('./components/digital-trends/pre/users-comments.php') ?>
+    <?php include('./components/digital-trends/pre/companies-list.php') ?>
+  </main>
+  <?php include('./components/footer.php'); ?>
+</body>
+
+</html>
---
+++ pages/digital-trends/pre/home-registrado.php
@@ -0,0 +1,29 @@
+<?php
+require_once('././src/components/cacheSettings.php');
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <?php include('./components/digital-trends/pre/home/head.php'); ?>
+  <?php include('./components/head.php'); ?>
+</head>
+
+<body class="emms__home">
+  <?php include('./components/gtm.php'); ?>
+  <?php include('./components/navbar-reg.php') ?>
+  <?php include('./components/share.php') ?>
+  <main>
+    <?php include('./components/digital-trends/pre/home/hello-module.php') ?>
+    <?php include('./components/digital-trends/pre/premium-content.php') ?>
+    <?php include('./components/digital-trends/pre/event-numbers.php') ?>
+    <?php include('./components/digital-trends/pre/speakers-carousel.php') ?>
+    <?php include('./components/digital-trends/pre/doppler-academy-banner.php'); ?>
+    <?php include('./components/digital-trends/pre/faqs.php') ?>
+    <?php include('./components/digital-trends/pre/companies-list.php') ?>
+  </main>
+  <?php include('./components/footer.php'); ?>
+</body>
+
+</html>
