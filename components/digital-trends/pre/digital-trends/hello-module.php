02d9084638944ccd6dd7d2bd632860cb3698d024
Matías Roy Rodriguez <mroy@makingsense.com>
2024-08-09 09:05:00 -0300

    feat: add new components styles

--- components/digital-trends/pre/digital-trends/hello-module.php
+++ components/digital-trends/pre/digital-trends/hello-module.php
@@ -1,58 +1,61 @@
 <section class="emms__hero-registration--registered">
-  <div class="emms__container--md">
-    <h1 class="emms__fade-top"><em>ONLINE Y GRATUITO - 02 Y 03 DE MAYO</em>¡Ya eres parte del EMMS Digital Trends 2024!</h1>
-    <p class="emms__fade-in">Te damos la bienvenida al evento pensado para tu Tienda Online con el que podrás inspirarte y aprender junto a especialistas. Guarda la fecha y mantente pendiente.</p>
-    <a href="https://www.addevent.com/event/fz20154258" target="_blank" class="emms__hero-registration__add-event emms__fade-in"><span>AGÉNDALO EN TU CALENDARIO</span></a>
-  </div>
-  <!-- Marquee -->
-  <div class="emms__hero-registration__bottom images emms__fade-in">
-    <p>
-      <img src="src/img/marquee/google.png" alt="Google">
-      <img class="sm" src="src/img/marquee/meta.png" alt="Meta">
-      <img src="src/img/marquee/youtube.png" alt="Youtube">
-      <img src="src/img/marquee/amazon.png" alt="Amazon">
-      <img src="src/img/marquee/metricool.png" alt="Metricool">
-      <img src="src/img/marquee/microsoft.png" alt="Microsoft">
-      <img class="sm" src="src/img/marquee/tiktok.png" alt="TikTok">
-      <img src="src/img/marquee/linkedin.png" alt="LinkedIn">
-      <img src="src/img/marquee/spotify.png" alt="Spotify">
-      <img src="src/img/marquee/vtex.png" alt="Vtex">
+    <div class="emms__container--md">
+        <h1 class="emms__fade-top"><em>ONLINE Y GRATUITO - DEL 26 AL 28 DE NOVIEMBRE</em>¡Felicitaciones! Ya eres parte
+            del EMMS DIGITAL TRENDS 2024:)</h1>
+        <p class="emms__fade-in">Te damos la bienvenida al evento donde descubrirás todas las tendencias de Marketing Digital. Agenda la fecha y mantente pendiente porque muy pronto vamos a publicar la agenda.</p>
+        <!-- TODO: Change link -->
+        <a href="https://www.addevent.com/event/fz20154258" target="_blank" class="emms__hero-registration__add-event emms__fade-in"><span>AGÉNDALO EN TU CALENDARIO</span></a>
+    </div>
+    <!-- TODO: Marquee component -->
+    <!-- Marquee -->
+    <div class="emms__hero-registration__bottom images emms__fade-in">
+        <p>
+            <img src="src/img/marquee/google.png" alt="Google">
+            <img class="sm" src="src/img/marquee/meta.png" alt="Meta">
+            <img src="src/img/marquee/youtube.png" alt="Youtube">
+            <img src="src/img/marquee/amazon.png" alt="Amazon">
+            <img src="src/img/marquee/metricool.png" alt="Metricool">
+            <img src="src/img/marquee/microsoft.png" alt="Microsoft">
+            <img class="sm" src="src/img/marquee/tiktok.png" alt="TikTok">
+            <img src="src/img/marquee/linkedin.png" alt="LinkedIn">
+            <img src="src/img/marquee/spotify.png" alt="Spotify">
+            <img src="src/img/marquee/vtex.png" alt="Vtex">
 
-      <!--  Repeated marquee items -->
-      <img src="src/img/marquee/google.png" alt="Google">
-      <img class="sm" src="src/img/marquee/meta.png" alt="Meta">
-      <img src="src/img/marquee/youtube.png" alt="Youtube">
-      <img src="src/img/marquee/amazon.png" alt="Amazon">
-      <img src="src/img/marquee/metricool.png" alt="Metricool">
-      <img src="src/img/marquee/microsoft.png" alt="Microsoft">
-      <img class="sm" src="src/img/marquee/tiktok.png" alt="TikTok">
-      <img src="src/img/marquee/linkedin.png" alt="LinkedIn">
-      <img src="src/img/marquee/spotify.png" alt="Spotify">
-      <img src="src/img/marquee/vtex.png" alt="Vtex">
+            <!--  Repeated marquee items -->
+            <img src="src/img/marquee/google.png" alt="Google">
+            <img class="sm" src="src/img/marquee/meta.png" alt="Meta">
+            <img src="src/img/marquee/youtube.png" alt="Youtube">
+            <img src="src/img/marquee/amazon.png" alt="Amazon">
+            <img src="src/img/marquee/metricool.png" alt="Metricool">
+            <img src="src/img/marquee/microsoft.png" alt="Microsoft">
+            <img class="sm" src="src/img/marquee/tiktok.png" alt="TikTok">
+            <img src="src/img/marquee/linkedin.png" alt="LinkedIn">
+            <img src="src/img/marquee/spotify.png" alt="Spotify">
+            <img src="src/img/marquee/vtex.png" alt="Vtex">
 
-      <!--  Repeated marquee items -->
-      <img src="src/img/marquee/google.png" alt="Google">
-      <img class="sm" src="src/img/marquee/meta.png" alt="Meta">
-      <img src="src/img/marquee/youtube.png" alt="Youtube">
-      <img src="src/img/marquee/amazon.png" alt="Amazon">
-      <img src="src/img/marquee/metricool.png" alt="Metricool">
-      <img src="src/img/marquee/microsoft.png" alt="Microsoft">
-      <img class="sm" src="src/img/marquee/tiktok.png" alt="TikTok">
-      <img src="src/img/marquee/linkedin.png" alt="LinkedIn">
-      <img src="src/img/marquee/spotify.png" alt="Spotify">
-      <img src="src/img/marquee/vtex.png" alt="Vtex">
+            <!--  Repeated marquee items -->
+            <img src="src/img/marquee/google.png" alt="Google">
+            <img class="sm" src="src/img/marquee/meta.png" alt="Meta">
+            <img src="src/img/marquee/youtube.png" alt="Youtube">
+            <img src="src/img/marquee/amazon.png" alt="Amazon">
+            <img src="src/img/marquee/metricool.png" alt="Metricool">
+            <img src="src/img/marquee/microsoft.png" alt="Microsoft">
+            <img class="sm" src="src/img/marquee/tiktok.png" alt="TikTok">
+            <img src="src/img/marquee/linkedin.png" alt="LinkedIn">
+            <img src="src/img/marquee/spotify.png" alt="Spotify">
+            <img src="src/img/marquee/vtex.png" alt="Vtex">
 
-      <!--  Repeated marquee items -->
-      <img src="src/img/marquee/google.png" alt="Google">
-      <img class="sm" src="src/img/marquee/meta.png" alt="Meta">
-      <img src="src/img/marquee/youtube.png" alt="Youtube">
-      <img src="src/img/marquee/amazon.png" alt="Amazon">
-      <img src="src/img/marquee/metricool.png" alt="Metricool">
-      <img src="src/img/marquee/microsoft.png" alt="Microsoft">
-      <img class="sm" src="src/img/marquee/tiktok.png" alt="TikTok">
-      <img src="src/img/marquee/linkedin.png" alt="LinkedIn">
-      <img src="src/img/marquee/spotify.png" alt="Spotify">
-      <img src="src/img/marquee/vtex.png" alt="Vtex">
-    </p>
-  </div>
+            <!--  Repeated marquee items -->
+            <img src="src/img/marquee/google.png" alt="Google">
+            <img class="sm" src="src/img/marquee/meta.png" alt="Meta">
+            <img src="src/img/marquee/youtube.png" alt="Youtube">
+            <img src="src/img/marquee/amazon.png" alt="Amazon">
+            <img src="src/img/marquee/metricool.png" alt="Metricool">
+            <img src="src/img/marquee/microsoft.png" alt="Microsoft">
+            <img class="sm" src="src/img/marquee/tiktok.png" alt="TikTok">
+            <img src="src/img/marquee/linkedin.png" alt="LinkedIn">
+            <img src="src/img/marquee/spotify.png" alt="Spotify">
+            <img src="src/img/marquee/vtex.png" alt="Vtex">
+        </p>
+    </div>
 </section>
