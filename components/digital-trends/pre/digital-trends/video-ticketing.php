a4a980bb737d97b294b1fb712f1056baa4eaba18
Matías Roy Rodriguez <mroy@makingsense.com>
2024-10-18 14:23:21 -0300

    feat: add video ticketing module

---
+++ components/digital-trends/pre/digital-trends/video-ticketing.php
@@ -0,0 +1,20 @@
+<section class="emms__centralvideo hidden--vip">
+    <div class="emms__centralvideo__head">
+        <h2>¡Quedan pocas entradas!</h2>
+        <span>Apúrate a reservar tu entrada VIP si…</span>
+    </div>
+    <div class="emms__container--lg reverse-mb">
+        <ul class="emms__centralvideo__list emms__centralvideo__list--live emms__fade-in">
+            <p class="emms__centralvideo__tag-play emms__centralvideo__tag-play--live">Dale play al video</p>
+            <li>Te has quedado sin ideas para crear contenido</li>
+            <li>Necesitas una asesoría personalizada </li>
+            <li>Sientes que el crecimiento de tu negocio se ha estancado</li>
+            <li>Necesitas aumentar tu visibilidad de marca</li>
+            <li>Buscas una experiencia inmersiva de Marketing</li>
+            <a href="#entradas" class="emms__cta">HAZTE VIP AHORA</a>
+        </ul>
+        <div class="emms__centralvideo__video lg emms__fade-in">
+            <video src="src/img/EmmsEcommerceNew.mp4" controls></video>
+        </div>
+    </div>
+</section>
