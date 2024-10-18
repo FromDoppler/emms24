449237bf7a241f4b0af5dd5c5e618120d04eb671
Matías Roy Rodriguez <mroy@makingsense.com>
2024-10-18 15:35:22 -0300

    chore: create marquee component

--- components/digital-trends/pre/digital-trends/hello-module.php
+++ components/digital-trends/pre/digital-trends/hello-module.php
@@ -5,56 +5,5 @@
         <p class="emms__fade-in">Te damos la bienvenida al evento donde descubrirás todas las tendencias de Marketing Digital. Agenda la fecha y mantente pendiente porque muy pronto vamos a publicar la agenda.</p>
         <a href="https://www.addevent.com/event/IM22579423" target="_blank" class="emms__hero-registration__add-event emms__fade-in"><span>AGÉNDALO EN TU CALENDARIO</span></a>
     </div>
-    <!-- TODO: Marquee component -->
-    <!-- Marquee -->
-    <div class="emms__hero-registration__bottom images emms__fade-in">
-        <p>
-            <img src="/src/img/marquee/google.png" alt="Google">
-            <img class="sm" src="/src/img/marquee/meta.png" alt="Meta">
-            <img src="/src/img/marquee/youtube.png" alt="Youtube">
-            <img src="/src/img/marquee/amazon.png" alt="Amazon">
-            <img src="/src/img/marquee/metricool.png" alt="Metricool">
-            <img src="/src/img/marquee/microsoft.png" alt="Microsoft">
-            <img class="sm" src="/src/img/marquee/tiktok.png" alt="TikTok">
-            <img src="/src/img/marquee/linkedin.png" alt="LinkedIn">
-            <img src="/src/img/marquee/spotify.png" alt="Spotify">
-            <img src="/src/img/marquee/vtex.png" alt="Vtex">
-
-            <!--  Repeated marquee items -->
-            <img src="/src/img/marquee/google.png" alt="Google">
-            <img class="sm" src="/src/img/marquee/meta.png" alt="Meta">
-            <img src="/src/img/marquee/youtube.png" alt="Youtube">
-            <img src="/src/img/marquee/amazon.png" alt="Amazon">
-            <img src="/src/img/marquee/metricool.png" alt="Metricool">
-            <img src="/src/img/marquee/microsoft.png" alt="Microsoft">
-            <img class="sm" src="/src/img/marquee/tiktok.png" alt="TikTok">
-            <img src="/src/img/marquee/linkedin.png" alt="LinkedIn">
-            <img src="/src/img/marquee/spotify.png" alt="Spotify">
-            <img src="/src/img/marquee/vtex.png" alt="Vtex">
-
-            <!--  Repeated marquee items -->
-            <img src="/src/img/marquee/google.png" alt="Google">
-            <img class="sm" src="/src/img/marquee/meta.png" alt="Meta">
-            <img src="/src/img/marquee/youtube.png" alt="Youtube">
-            <img src="/src/img/marquee/amazon.png" alt="Amazon">
-            <img src="/src/img/marquee/metricool.png" alt="Metricool">
-            <img src="/src/img/marquee/microsoft.png" alt="Microsoft">
-            <img class="sm" src="/src/img/marquee/tiktok.png" alt="TikTok">
-            <img src="/src/img/marquee/linkedin.png" alt="LinkedIn">
-            <img src="/src/img/marquee/spotify.png" alt="Spotify">
-            <img src="/src/img/marquee/vtex.png" alt="Vtex">
-
-            <!--  Repeated marquee items -->
-            <img src="/src/img/marquee/google.png" alt="Google">
-            <img class="sm" src="/src/img/marquee/meta.png" alt="Meta">
-            <img src="/src/img/marquee/youtube.png" alt="Youtube">
-            <img src="/src/img/marquee/amazon.png" alt="Amazon">
-            <img src="/src/img/marquee/metricool.png" alt="Metricool">
-            <img src="/src/img/marquee/microsoft.png" alt="Microsoft">
-            <img class="sm" src="/src/img/marquee/tiktok.png" alt="TikTok">
-            <img src="/src/img/marquee/linkedin.png" alt="LinkedIn">
-            <img src="/src/img/marquee/spotify.png" alt="Spotify">
-            <img src="/src/img/marquee/vtex.png" alt="Vtex">
-        </p>
-    </div>
+    <?php include($_SERVER['DOCUMENT_ROOT'] . '/components/marquee.php') ?>
 </section>
--- components/digital-trends/pre/register-withoutform.php
+++ components/digital-trends/pre/register-withoutform.php
@@ -18,58 +18,7 @@
         </div>
     </div>
     <button class="emms__cta digitalWithoutForm" type="submit" id="digitalWithoutForm"><span class="button__text">Regístrate gratis</span></button>
-    <div class="emms__hero-registration__bottom images emms__fade-in">
-        <p>
-            <img src="/src/img/marquee/google.png" alt="Google">
-            <img class="sm" src="/src/img/marquee/meta.png" alt="Meta">
-            <img src="/src/img/marquee/youtube.png" alt="Youtube">
-            <img src="/src/img/marquee/amazon.png" alt="Amazon">
-            <img src="/src/img/marquee/metricool.png" alt="Metricool">
-            <img src="/src/img/marquee/microsoft.png" alt="Microsoft">
-            <img class="sm" src="/src/img/marquee/tiktok.png" alt="TikTok">
-            <img src="/src/img/marquee/linkedin.png" alt="LinkedIn">
-            <img src="/src/img/marquee/spotify.png" alt="Spotify">
-            <img src="/src/img/marquee/vtex.png" alt="Vtex">
-
-            <!--  Repeated marquee items -->
-            <img src="/src/img/marquee/google.png" alt="Google">
-            <img class="sm" src="/src/img/marquee/meta.png" alt="Meta">
-            <img src="/src/img/marquee/youtube.png" alt="Youtube">
-            <img src="/src/img/marquee/amazon.png" alt="Amazon">
-            <img src="/src/img/marquee/metricool.png" alt="Metricool">
-            <img src="/src/img/marquee/microsoft.png" alt="Microsoft">
-            <img class="sm" src="/src/img/marquee/tiktok.png" alt="TikTok">
-            <img src="/src/img/marquee/linkedin.png" alt="LinkedIn">
-            <img src="/src/img/marquee/spotify.png" alt="Spotify">
-            <img src="/src/img/marquee/vtex.png" alt="Vtex">
-
-            <!--  Repeated marquee items -->
-            <img src="/src/img/marquee/google.png" alt="Google">
-            <img class="sm" src="/src/img/marquee/meta.png" alt="Meta">
-            <img src="/src/img/marquee/youtube.png" alt="Youtube">
-            <img src="/src/img/marquee/amazon.png" alt="Amazon">
-            <img src="/src/img/marquee/metricool.png" alt="Metricool">
-            <img src="/src/img/marquee/microsoft.png" alt="Microsoft">
-            <img class="sm" src="/src/img/marquee/tiktok.png" alt="TikTok">
-            <img src="/src/img/marquee/linkedin.png" alt="LinkedIn">
-            <img src="/src/img/marquee/spotify.png" alt="Spotify">
-            <img src="/src/img/marquee/vtex.png" alt="Vtex">
-
-            <!--  Repeated marquee items -->
-            <img src="/src/img/marquee/google.png" alt="Google">
-            <img class="sm" src="/src/img/marquee/meta.png" alt="Meta">
-            <img src="/src/img/marquee/youtube.png" alt="Youtube">
-            <img src="/src/img/marquee/amazon.png" alt="Amazon">
-            <img src="/src/img/marquee/metricool.png" alt="Metricool">
-            <img src="/src/img/marquee/microsoft.png" alt="Microsoft">
-            <img class="sm" src="/src/img/marquee/tiktok.png" alt="TikTok">
-            <img src="/src/img/marquee/linkedin.png" alt="LinkedIn">
-            <img src="/src/img/marquee/spotify.png" alt="Spotify">
-            <img src="/src/img/marquee/vtex.png" alt="Vtex">
-        </p>
+    <?php include($_SERVER['DOCUMENT_ROOT'] . '/components/marquee.php') ?>
     </div>
-    </div>
-    <!-- TODO: Marquee component -->
-    <!-- Marquee -->
 
 </section>
