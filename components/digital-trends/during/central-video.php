3e147808428c06029066017fb0f68a4d27961390
Matías Roy Rodriguez <mroy@makingsense.com>
2024-11-19 15:02:12 -0300

    feat: create and replace central-video live module

--- components/digital-trends/during/central-video.php
+++ components/digital-trends/during/central-video.php
@@ -2,29 +2,35 @@
 
 $contents = [
     '/' => [
-        'heading' => 'Súmate al EMMS Digital Trends 2024 y entérate las últimas innovaciones en Marketing Digital',
+        'heading' => 'Súmate ahora al EMMS Digital Trends',
+        'subHeading' => 'Inspírate y capacítate con los mayores <br>
+especialistas en Marketing Digital. ¡Ya empezó!',
         'body' => 'Descubre en este video por qué miles de profesionales y referentes en la industria eligen este evento para capacitarse.',
-        'button' => 'RESERVA TU CUPO GRATIS',
+        'button' => 'ACCEDE AL VIVO',
         'link' => 'digital-trends#registro',
         'youtubeCode' => 'ZGS-lmiXHFE',
     ],
     '/digital-trends' => [
-        'heading' => 'Inspírate con el mayor evento hispano de Marketing Digital',
-        'body' => 'Conoce en este video qué hace al EMMS Digital Trends el lugar ideal para capacitarte
-y aprender cómo hacer crecer tu negocio junto a los líderes del sector.',
-        'button' => 'REGÍSTRATE AHORA',
+        'heading' => 'Súmate ahora al EMMS Digital Trends',
+        'subHeading' => 'Inspírate y capacítate con los mayores <br>
+        especialistas en Marketing Digital. ¡Ya empezó!',
+        'body' => 'Descubre en este video por qué miles de profesionales y referentes en la industria eligen este evento para capacitarse.',
+        'button' => 'ACCEDE AL VIVO',
         'link' => '#registro',
         'youtubeCode' => 'ZGS-lmiXHFE',
     ],
     '/digital-trends-registrado' => [
         'heading' => '¡Gracias por sumarte al EMMS Digital Trends 2024!',
+        'subHeading' => '',
         'body' => 'Comenzó la cuenta regresiva para que descubras las últimas innovaciones en Marketing Digital',
         'youtubeCode' => 'isDPHOi2mAs',
     ],
     '/*' => [
-        'heading' => 'Súmate al EMMS Digital Trends 2024 y entérate las últimas innovaciones en Marketing Digital',
+        'heading' => 'Súmate ahora al EMMS Digital Trends',
+        'subHeading' => 'Inspírate y capacítate con los mayores <br>
+        especialistas en Marketing Digital. ¡Ya empezó!',
         'body' => 'Descubre en este video por qué miles de profesionales y referentes en la industria eligen este evento para capacitarse.',
-        'button' => 'RESERVA TU CUPO GRATIS',
+        'button' => 'ACCEDE AL VIVO',
         'link' => 'digital-trends#registro',
         'youtubeCode' => 'ZGS-lmiXHFE',
     ],
@@ -44,8 +50,9 @@ $videoUrl = $youtubeBaseUrl . $content['youtubeCode'] . $youtubeParams;
     <div class="emms__container--lg emms__container--lg--column">
         <?php if (!empty($content['heading']) || !empty($content['body'])): ?>
             <div class="emms__centralvideo__title emms__fade-in">
-                <?php if (!empty($content['heading'])): ?>
+                <?php if (!empty($content['heading']) && !empty($content['subHeading'])): ?>
                     <h2><?php echo $content['heading']; ?></h2>
+                    <h2><?php echo $content['subHeading']; ?></h2>
                 <?php endif; ?>
                 <?php if (!empty($content['body'])): ?>
                     <p><?php echo $content['body']; ?></p>
