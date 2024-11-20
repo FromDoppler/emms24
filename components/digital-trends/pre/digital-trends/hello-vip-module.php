098038fd93522e9468e4abe3d6d317a0301ce861
Matias Rodriguez <mroy@makingsense.com>
2024-11-20 16:14:13 -0300

    chore: change hello vip module content

--- components/digital-trends/pre/digital-trends/hello-vip-module.php
+++ components/digital-trends/pre/digital-trends/hello-vip-module.php
@@ -1,8 +1,8 @@
 <section class="emms__hero-registration--registered digital--trends">
     <div class="emms__container--md">
-        <h1 class="emms__fade-top"><em>¡YA ERES PARTE DEL EMMS DIGITAL TRENDS 2024!</em>Ahora, el siguiente paso es... <br>
-            ¡Tu Entrada VIP!</h1>
-        <p class="emms__fade-in">Anímate a vivir toda la experiencia sumándote a los Workshops y espacios de Networking con tu entrada VIP. Accederás a contactos e ideas prácticas para implementar cuanto antes en tu negocio. ¡Y por sólo 9,99 USD!</p>
+        <h1 class="emms__fade-top"><em>TE DAMOS LA BIENVENIDA AL EVENTO MÁS IMPORTANTE DE MARKETING DIGITAL</em>Felicitaciones, ¡Ya eres parte <br>
+        del EMMS Digital Trends 2024!</h1>
+        <p class="emms__fade-in">Ya tienes tu Entrada Free y tienes acceso a todas las Conferencias gratuitas. Puedes comprar ahora una Entrada VIP y aprovechar también los espacios exclusivos de Workshops y Networking</p>
         <a href="#entradas" class="emms__purple-anchor emms__fade-in">Descubre todos los beneficios</a>
         <a class="emms__cta emms__fade-in" href="./checkout">COMPRA TU ENTRADA VIP AHORA</a>
     </div>
