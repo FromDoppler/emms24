ffda22719e40b09a799f121a58f275fdd6ff5404
Matias Rodriguez <mroy@makingsense.com>
2024-11-21 12:16:58 -0300

    chore: change calendar content

--- components/digital-trends/schedule/schedule.php
+++ components/digital-trends/schedule/schedule.php
@@ -23,10 +23,17 @@ $block = getBlock($normalizedUrl);
 <section class="emms__calendar" id="agenda">
     <div class="emms__container--lg">
         <div class="emms__calendar__title emms__fade-in">
-            <h2>Agenda EMMS Digital Trends 2024</h2>
-            <p class="hidden--vip">Del 26 al 28 de noviembre podrás disfrutar de Conferencias y Workshops, como así también
-                de un espacio de Networking diseñado especialmente para que puedas conectar y proyectar colaboraciones con especialistas de Marketing Digital.</p>
-            <p class="show--vip">Descubre aquí los Speakers internacionales y las actividades exclusivas que te esperarán en esta edición. <br> Conferencias, Workshops, Casos de Éxito, Networking ¡y muchos más!</p>
+
+            <?php if ($digitalTrendsStates['isPre']) : ?>
+                <h2>Agenda EMMS Digital Trends 2024</h2>
+                <p class="hidden--vip">Del 26 al 28 de noviembre podrás disfrutar de Conferencias y Workshops, como así también
+                    de un espacio de Networking diseñado especialmente para que puedas conectar y proyectar colaboraciones con especialistas de Marketing Digital.</p>
+                <p class="show--vip">Descubre aquí los Speakers internacionales y las actividades exclusivas que te esperarán en esta edición. <br> Conferencias, Workshops, Casos de Éxito, Networking ¡y muchos más!</p>
+            <?php elseif ($digitalTrendsStates['isDuring']) : ?>
+                <h2>Agenda EMMS Digital Trends 2024</h2>
+                <p>Del 26 al 28 de noviembre podrás disfrutar de Conferencias, Workshops y Networking</p>
+                <p><strong>Importante:</strong> todos los Workshops quedarán grabados. Elige cada día el espacio de trabajo que más te guste para presenciar en vivo ¡y podrás revivir luego los otros!</p>
+            <?php endif ?>
         </div>
         <?php include('speakers.php') ?>
 
