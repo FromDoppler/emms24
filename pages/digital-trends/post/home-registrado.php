3072a4787f6567bf6fb08326907661d99dd1803b
Matias Rodriguez <mroy@makingsense.com>
2024-11-26 14:41:51 -0300

    feat: add post home first module

---
+++ pages/digital-trends/post/home-registrado.php
@@ -0,0 +1,31 @@
+<?php
+require_once($_SERVER['DOCUMENT_ROOT'] . '/src/components/cacheSettings.php');
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <?php include($_SERVER['DOCUMENT_ROOT'] . '/components/digital-trends/during/home/head.php'); ?>
+    <?php include($_SERVER['DOCUMENT_ROOT'] . '/components/head.php'); ?>
+</head>
+
+<body class="emms__home">
+    <?php include($_SERVER['DOCUMENT_ROOT'] . '/components/gtm.php'); ?>
+    <?php include($_SERVER['DOCUMENT_ROOT'] . '/components/hello-bar.php') ?>
+    <?php include($_SERVER['DOCUMENT_ROOT'] . '/components/navbar-reg.php') ?>
+    <?php include($_SERVER['DOCUMENT_ROOT'] . '/components/share.php') ?>
+    <main>
+        <?php include($_SERVER['DOCUMENT_ROOT'] . '/components/digital-trends/post/home/hello-module.php');   ?>
+        <?php include($_SERVER['DOCUMENT_ROOT'] . '/components/digital-trends/during/central-video.php'); ?>
+        <?php include($_SERVER['DOCUMENT_ROOT'] . '/components/digital-trends/pre/event-numbers.php') ?>
+        <?php include($_SERVER['DOCUMENT_ROOT'] . '/components/digital-trends/pre/speakers-carousel.php') ?>
+        <?php include($_SERVER['DOCUMENT_ROOT'] . '/components/digital-trends/pre/premium-content.php') ?>
+        <?php include($_SERVER['DOCUMENT_ROOT'] . '/components/digital-trends/pre/doppler-academy-banner.php'); ?>
+        <?php include($_SERVER['DOCUMENT_ROOT'] . '/components/digital-trends/during/faqs.php') ?>
+        <?php include($_SERVER['DOCUMENT_ROOT'] . '/components/digital-trends/pre/companies-list.php') ?>
+    </main>
+    <?php include($_SERVER['DOCUMENT_ROOT'] . '/components/footer.php'); ?>
+</body>
+
+</html>
