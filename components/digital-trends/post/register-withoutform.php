7a9e5315cc6a35990698bcceb49a611bfacec74f
Matias Rodriguez <mroy@makingsense.com>
2024-11-28 11:21:12 -0300

    chore: add register without form module

---
+++ components/digital-trends/post/register-withoutform.php
@@ -0,0 +1,25 @@
+<!-- FIXME: A la espera de contenido final -->
+<section class="emms__hero-registration--registered digital--trends">
+    <div class="emms__container--md">
+        <h1 class="emms__fade-in">¡Ya empezó el DIGITAL TRENDS 2024!</h1>
+        <p class="emms__fade-in">Súmate al evento exclusivo pensado para que conozcas de la mano de expertos y referentes internacionales todas las tendencias del Marketing Digital.</p>
+        <div class="text-with-icon-container">
+            <div class="text-with-icon">
+                <img src="/src/img/icons/icon-check--strong-purple.svg" alt="Check icon">
+                <span>Speakers nternacionales</span>
+            </div>
+            <div class="text-with-icon">
+                <img src="/src/img/icons/icon-check--strong-purple.svg" alt="Check icon">
+                <span>HERRAMIENTAS Y RECURSOS</span>
+            </div>
+            <div class="text-with-icon">
+                <img src="/src/img/icons/icon-check--strong-purple.svg" alt="Check icon">
+                <span>WORKSHOP Y NETWORKING</span>
+            </div>
+        </div>
+    </div>
+    <button class="emms__cta digitalWithoutForm" type="submit" id="digitalWithoutForm"><span class="button__text">Regístrate gratis</span></button>
+    <?php include($_SERVER['DOCUMENT_ROOT'] . '/components/marquee.php') ?>
+    </div>
+
+</section>
